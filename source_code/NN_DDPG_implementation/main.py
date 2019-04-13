5970497863996010b8d101f0abacf79985ddb4d9
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-04-13 23:13:45 +0200

    Simulation added

--- source_code/NN_DDPG_implementation/main.py
+++ source_code/NN_DDPG_implementation/main.py
@@ -7,17 +7,19 @@ import torch
 from AgentDDPG import AgentDDPG
 from NormalizedActions import NormalizedActions
 from OUNoise import OUNoise
+from Simulation import Simulation
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='State implementation of DDPG.',
                                      epilog="No example of usage")
     # Environment
+    parser.add_argument('-simulation_actor', nargs=2, default=[False, ''], metavar=('boolean', 'path'))
     parser.add_argument('-env', default='MountainCarContinuous-v0', type=str, help='Name of the Gym Environment')
     # OUNoise (mu=0.0, sigma=0.3, theta=0.15)
     parser.add_argument('-noise', nargs=3, default=[0.0, 0.3, 0.15], metavar=('mu', 'sigma', 'theta'), type=float,
                         help='Ornstein Uhlenbeck process noise parameters')
-    parser.add_argument('-eps', nargs=3, default=[0.99, 0.2, 500], metavar=('start', 'end', 'decay'), type=float,
+    parser.add_argument('-eps', nargs=3, default=[0.9, 0.2, 300], metavar=('start', 'end', 'decay'), type=float,
                         help='Epsilon Decay process to decay the noise')
     parser.add_argument('-replay', nargs=3, default=[100, 10000, 1000000],
                         metavar=('batch_size', 'replay_min_size', 'replay_max_size'), type=int,
@@ -38,23 +40,33 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
     print(args)
-    plt.ion()
-    
-    # if gpu is to be used
-    assert torch.cuda.is_available(), "CUDA is not available"
-    for i in range(20):
+    if args.simulation_actor[0]:
         env = NormalizedActions(gym.make(args.env))
-        test_env = NormalizedActions(gym.make(args.env))
-        ou_noise = OUNoise(env.action_space, mu=args.noise[0], sigma=args.noise[1], theta=args.noise[2])
-    
-        ddpg = AgentDDPG(env, test_env, exp_strategy=ou_noise,
-                         eps_start=args.eps[0], eps_end=args.eps[1], eps_decay=args.eps[2],
-                         batch_size=args.replay[0], replay_min_size=args.replay[1], replay_max_size=args.replay[2],
-                         n_episode=args.sim[0], episode_max_len=args.sim[1],
-                         actor_weight_decay=args.actor[0], actor_update_method=args.actor[1], actor_lr=args.actor[2],
-                         critic_weight_decay=args.critic[0], critic_update_method=args.critic[1],
-                         critic_lr=args.critic[2],
-                         discount=args.update[0], soft_target_tau=args.update[1], n_updates_per_sample=args.update[2],
-                         eval_samples=args.test, tensorboard_dir='./replay-100/', run=i)
-        ddpg.train()
+        sim = Simulation(env, args.simulation_actor[1])
+        sim.simulate()
         env.close()
+    else:
+        plt.ion()
+    
+        # if gpu is to be used
+        assert torch.cuda.is_available(), "CUDA is not available"
+        for i in range(20):
+            env = NormalizedActions(gym.make(args.env))
+            test_env = NormalizedActions(gym.make(args.env))
+            env.env._max_episode_steps = args.sim[1]
+            test_env.env._max_episode_steps = args.sim[1]
+            ou_noise = OUNoise(env.action_space, mu=args.noise[0], sigma=args.noise[1], theta=args.noise[2])
+        
+            ddpg = AgentDDPG(env, test_env, exp_strategy=ou_noise,
+                             eps_start=args.eps[0], eps_end=args.eps[1], eps_decay=args.eps[2],
+                             batch_size=args.replay[0], replay_min_size=args.replay[1], replay_max_size=args.replay[2],
+                             n_episode=args.sim[0], episode_max_len=args.sim[1],
+                             actor_weight_decay=args.actor[0], actor_update_method=args.actor[1],
+                             actor_lr=args.actor[2],
+                             critic_weight_decay=args.critic[0], critic_update_method=args.critic[1],
+                             critic_lr=args.critic[2],
+                             discount=args.update[0], soft_target_tau=args.update[1],
+                             n_updates_per_sample=args.update[2],
+                             eval_samples=args.test, tensorboard_dir='./replay-100-mountain/', run=i)
+            ddpg.train()
+            env.close()
