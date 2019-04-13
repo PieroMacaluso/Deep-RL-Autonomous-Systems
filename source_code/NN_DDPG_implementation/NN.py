a3ff3cdb68da05d20d300dd7a56e4ef94118ffba
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-04-13 23:14:10 +0200

    NN tweaked
    (cherry picked from commit 483f58e8a516e4b2e5de9a1d30249a9426a4aacb)

--- source_code/NN_DDPG_implementation/NN.py
+++ source_code/NN_DDPG_implementation/NN.py
@@ -4,22 +4,14 @@ from torch import nn
 
 
 class CriticNN(nn.Module):
-    """
-    Value Network
-    """
-    
-    def __init__(self, num_inputs, num_actions, hidden_size=256, init_w=3e-3):
+    def __init__(self, num_inputs, num_actions, hidden_size=256):
         super(CriticNN, self).__init__()
-        
         self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, 1)
-        
-        self.linear3.weight.data.uniform_(-init_w, init_w)
-        self.linear3.bias.data.uniform_(-init_w, init_w)
     
     def forward(self, state, action):
-        x = torch.cat([state, action], 1)
+        x = torch.cat((state, action), 1)
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
         x = self.linear3(x)
@@ -27,22 +19,14 @@ class CriticNN(nn.Module):
 
 
 class ActorNN(nn.Module):
-    """
-    Policy Network
-    """
-    
-    def __init__(self, num_inputs, num_actions, hidden_size=256, init_w=3e-3):
+    def __init__(self, num_inputs, num_actions, hidden_size=256):
         super(ActorNN, self).__init__()
-        
         self.linear1 = nn.Linear(num_inputs, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
         self.linear3 = nn.Linear(hidden_size, num_actions)
-        
-        self.linear3.weight.data.uniform_(-init_w, init_w)
-        self.linear3.bias.data.uniform_(-init_w, init_w)
 
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
-        x = F.tanh(self.linear3(x))
+        x = torch.tanh(self.linear3(x))
         return x
