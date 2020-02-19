e339e4a9d2d1e74a3d24dabd37712d2c504b0004
Piero Macaluso <piero.macaluso@studenti.polito.it>
2020-02-19 23:08:23 +0100

    [CozmoDriver] Gitignore Fix e Docs

--- gym-cozmo/gym_cozmo/envs/cozmo_env.py
+++ gym-cozmo/gym_cozmo/envs/cozmo_env.py
@@ -16,13 +16,26 @@ MAX_T_SPEED = 100
 
 
 class CozmoEnv(gym.Env):
+    '''
+    Cozmo Environment Class
+    '''
+    
     metadata = {'render.modes': ['human']}
     
     def __init__(self, robot: cozmo.robot.Robot, img_h: int, img_w: int):
+        """
+        Initialisation of environment parameters.
+        @param robot: structure of Cozmo Robot provided by its SDK
+        @type robot: cozmo.robot.Robot
+        @param img_h: image height
+        @type img_h: int
+        @param img_w: image_width
+        @type img_w: int
+        """
         # choice_time: time between an action and the next one
         # this value is equal to 1 / number_frame_second
         # number_frame_second is generally up to 15 as reported in Cozmo SDK
-        self.choice_time = 1/15
+        self.choice_time = 1 / 15
         self.last_action = None
         self.seed()
         self.img_h = img_h
@@ -39,14 +52,19 @@ class CozmoEnv(gym.Env):
                                dtype=np.float32)
         self.action_space = spaces.Box(np.array([0, -1]), np.array([+1, +1]), dtype=np.float32)
         self.observation_space = spaces.Box(low=0, high=1, shape=(self.img_h, self.img_w), dtype=np.float32)
-        # self.say("All set!")
     
     def step(self, action: spaces.Box):
+        """
+        Make a step in the environment
+        @param action: action selected by the current policy (outside)
+        @type action: spaces.Box
+        @return: the next state, the reward of the actions just made, the done flag, other information
+        @rtype: image, number, boolean, any
+        """
         now_time = time.time()
         step_reward = -1
         if action is not None:
             self.drive(action)
-            # TODO: select choice_time or last_time
             step_reward = action[0] * MAX_F_SPEED * self.choice_time
             self.last_time = now_time
         
@@ -66,6 +84,11 @@ class CozmoEnv(gym.Env):
         return self.state, step_reward, done, {}
     
     def reset(self):
+        """
+        Reset the current environment by returning the reset state
+        @return: reset state
+        @rtype: image
+        """
         # self.say("New Episode!")
         self.start_position()
         self.reward = 0.0
@@ -74,16 +97,46 @@ class CozmoEnv(gym.Env):
         return self.state
     
     def seed(self, seed=None):
+        """
+        Setup the seed for the current environment
+        @param seed: seed
+        @type seed: int
+        @return:
+        @rtype:
+        """
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
     
     def render(self, mode='human', close=False):
+        """
+        Unused method
+        @param mode:
+        @type mode:
+        @param close:
+        @type close:
+        @return:
+        @rtype:
+        """
         pass
     
     def set_lift_height(self, height):
+        """
+        Set the height of the lift of Cozmo
+        @param height:
+        @type height:
+        @return:
+        @rtype:
+        """
         self.robot.set_lift_height(height).wait_for_completed()
     
     def set_head_angle(self, degrees):
+        """
+        Set the head position of Cozmo
+        @param degrees:
+        @type degrees:
+        @return:
+        @rtype:
+        """
         angle = Angle(degrees=degrees)
         self.robot.set_head_angle(angle).wait_for_completed()
         pass
@@ -92,12 +145,17 @@ class CozmoEnv(gym.Env):
         self.start_position()
     
     def start_position(self):
-        self.set_head_angle(self.head.low+2)
+        self.set_head_angle(self.head.low + 2)
         self.set_lift_height(self.lift.high)
     
     def get_image(self):
+        """
+        Get image from Cozmo Camera and do the proper transformation
+        @return: cozmo camera image
+        @rtype: image
+        """
         observation = self.robot.world.latest_image.raw_image
-
+        
         while observation is None:
             observation = self.robot.world.latest_image.raw_image
         # Returned screen requested by gym is HWC. Transpose it into torch order (CHW).\
@@ -113,15 +171,34 @@ class CozmoEnv(gym.Env):
         return screen
     
     def drive(self, action: spaces.Box):
+        """
+        Formula to convert actions to values understan
+        @param action:
+        @type action:
+        @return:
+        @rtype:
+        """
         l_wheel_speed = action[0] * MAX_F_SPEED + action[1] * MAX_T_SPEED
         r_wheel_speed = action[0] * MAX_F_SPEED - action[1] * MAX_T_SPEED
         
         self.robot.drive_wheels(l_wheel_speed, r_wheel_speed, l_wheel_speed * 4, r_wheel_speed * 4)
     
     def say(self, message):
+        """
+        Wrapper to make Cozmo say something. It is used in the project only to feedback different phases
+        @param message: Message to make Cozmo say
+        @type message: string
+        @return: nothing
+        @rtype: nothing
+        """
         self.robot.say_text(message).wait_for_completed()
     
     def is_human_controlled(self):
+        """
+        Return true if the human has control, false otherwise
+        @return: result of the operation
+        @rtype: 
+        """
         return self.rc.is_human_controlled()
     
     def is_forget_enabled(self):
